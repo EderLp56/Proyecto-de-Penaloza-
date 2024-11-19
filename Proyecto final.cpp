8fcce39e62f18d777ec4a0e889b4d435fae322c8
EderLp56 <ederlopez3070@gmail.com>
2024-11-18 18:07:57 -0600

    Update Proyecto final.cpp

--- Proyecto final.cpp
+++ Proyecto final.cpp
@@ -117,4 +117,12 @@ void ListaCandidatas(char szPalabrasSugeridas[][TAMTOKEN], int iNumSugeridas, ch
 
 #define TAMTOKEN 50         // Tamaño máximo de cada palabra
 #define MAXPALABRAS 1000     // Número máximo de palabras en el diccionario
+// Prototipos de las funciones
+void Diccionario(char *szNombre, char szPalabras[][TAMTOKEN], int iEstadisticas[], int *iNumElementos);
+void ClonaPalabras(char *szPalabraLeida, char szPalabrasSugeridas[][TAMTOKEN], int *iNumSugeridas);
+void ListaCandidatas(char szPalabrasSugeridas[][TAMTOKEN], int iNumSugeridas, char szPalabras[][TAMTOKEN],
+                     int iEstadisticas[], int iNumElementos, char szListaFinal[][TAMTOKEN], int iPeso[], int *iNumLista);
+
+#endif // CORRECTOR_H
+
 
